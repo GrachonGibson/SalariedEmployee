dbcb8fc7073fc4d51da5561a588d96fc4fdb1aec
DaringShadows <grachonjr@gmail.com>
2019-04-03 09:46:15 -0500

    Group Project

---
+++ src/main/java/SalariedEmployee.java
@@ -0,0 +1,23 @@
+public class SalariedEmployee extends Employee {
+
+    private double weeklySalary;
+
+
+    void setWeeklySalary(double ws){
+        weeklySalary = ws;
+    }
+
+    double getWeeklySalary(){
+        return weeklySalary;
+    }
+    public String getFullName(){
+        String fullName = getFirstName() + getLastName();
+        return fullName;
+    }
+    @Override
+    public String calculatePay(){
+        String str = "";
+        double totalPay = weeklySalary * 52;
+        str = str + getFullName() + " makes $" + totalPay + " annually.";
+        return str;
+    }
